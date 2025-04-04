3daf2961ef8a1902e36246e9f5ace7fd56eb778b
minikids <96962258+minikids@users.noreply.github.com>
2025-04-03 18:38:01 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, request
+from flask_cors import CORS  # Import CORS
 import os
 
 app = Flask(__name__)
+
+# Enable CORS for the entire app
+CORS(app)
 
 # Accessing the environment variable using os.environ
 bignum = os.environ.get('BIG_NUMBER')  # Make sure BIG_NUMBER is set in your environment
